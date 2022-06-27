3095f4a8430504e924257c4128f4e773e6e45e70
Flavio Ever <flavio.everb@gmail.com>
2022-06-27 14:15:04 -0300

    fix stylesheet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,16 +23,6 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="apple-mobile-web-app-status-bar-style"
           content="black-translucent"
         />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;500&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       <GlobalStyles />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,6 +40,16 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;500&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
