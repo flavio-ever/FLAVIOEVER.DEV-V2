8b7865ab0082ab16baedf9639b1204ed38c7e2bf
Flavio Ever <flavio.everb@gmail.com>
2022-07-11 09:44:18 -0300

    tdd subfolders

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules', '/.next'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/components/*.ts(x)', 'src/templates/*.ts(x)'],
+  collectCoverageFrom: [
+    'src/components/**/*.ts(x)',
+    'src/templates/**/*.ts(x)'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
